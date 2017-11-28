50a59a1cc28c776e65130a61ea7d3da150e9a9d9
ding <playdingnow@gmail.com>
2017-11-28 10:00:20 +0800

    feat(scripts): add alias

--- i-docker-compose.sh
+++ i-docker-compose.sh
@@ -1,2 +1,8 @@
 curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+
+rcfile='~/.bashrc'
+echo 'alias d=docker\n' >> $rcfile
+echo 'alias dc=docker-compose\n' >> $rcfile
+source $rcfile
