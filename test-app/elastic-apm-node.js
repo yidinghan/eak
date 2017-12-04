6d1ecc7da8c0bb96ac911889b8e69d888ca96450
ding <playdingnow@gmail.com>
2017-12-04 17:19:39 +0800

    feat(app): set captureTraceStackTracesedit to false

--- test-app/elastic-apm-node.js
+++ test-app/elastic-apm-node.js
@@ -1,12 +1,14 @@
 module.exports = {
-  active: true,
-  // Set required app name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  appName: 'apm-reseach',
+    active: true,
+    // Set required app name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+    appName: 'apm-reseach',
 
-  // Use if APM Server requires a token
-  secretToken:
-    'xxVpmQB2HMzCL9PgBHVrnxjNXXw5J7bd79DFm6sjBJR5HPXDhcF8MSb3vv4bpg44',
+    // Use if APM Server requires a token
+    secretToken: 'xxVpmQB2HMzCL9PgBHVrnxjNXXw5J7bd79DFm6sjBJR5HPXDhcF8MSb3vv4bpg44',
 
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: `http://${process.env.apmserver}:8200`,
+    // Set custom APM Server URL (default: http://localhost:8200)
+    serverUrl: `http://${process.env.apmserver}:8200`,
+
+    // Set this option to false to disable capture of stack traces for measured traces during instrumentation.
+    captureTraceStackTracesedit: false,
 };
