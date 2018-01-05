5c8699822499f37efd08c43812847d7646adbebd
ding <playdingnow@gmail.com>
2018-01-05 13:23:17 +0800

    feat(scripts): upgrade compose to 1.18

--- i-docker-compose.sh
+++ i-docker-compose.sh
@@ -1,8 +1,8 @@
-curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
-rcfile='/root/.bashrc'
+rcfile='~/.bashrc'
 echo 'alias d=docker' >> $rcfile
 echo 'alias dc=docker-compose' >> $rcfile
 
